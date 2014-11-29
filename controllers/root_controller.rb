7e9a7b6122a0651cd407992a5bde44a663f4677c
ericak11 <kantor.erica@gmail.com>
2014-11-29 16:08:33 -0500

    spell checking works

--- controllers/root_controller.rb
+++ controllers/root_controller.rb
@@ -4,20 +4,34 @@ class RootController < ApplicationController
   end
 
   post('/') do
-    input_array = format_input(params[:text])
-    check_dictionary
+    results = check_dictionary(params[:text])
+    total_words = results[1] + results[2]
+    accuracy  = (results[1]/total_words.to_f) * 100
+    phrase = results[0]
+    {phrase: phrase, accuracy: accuracy, incorrect: results[2], correct: results[1], total: total_words}.to_json
   end
 
-  def format_input(text)
-    text.gsub(/[?.,:;'"!&()]/, "").split(" ")
-  end
-
-  def check_dictionary
-    dict = []
-    File.open('./public/images/dictionary.txt').each do |row|
-      dict.push(row.strip.downcase)
+  def check_dictionary(text)
+    phrase = ""
+    correct = 0
+    incorrect = 0
+    dict = Hash.new{|hash, key| hash[key] = []}
+    File.open('public/images/dictionary.txt').each do |row|
+      word = row.strip.downcase
+      first_letter =  word[0,1]
+      dict[first_letter] << word
     end
-    dict
+    text.split(" ").each do |word|
+      adjusted_word = word.gsub(/[?.,:;'"!&()]/, "").downcase
+      if dict[adjusted_word[0]].include? adjusted_word
+        phrase += "#{word} "
+        correct += 1
+      else
+        phrase += "<span class='wrong'>#{word}</span> "
+        incorrect += 1
+      end
+    end
+    [phrase, correct, incorrect]
   end
 
 end
