28c5e170132fbae438bf3e8636a3a3b5bea959f7
ericak11 <kantor.erica@gmail.com>
2014-11-29 14:44:36 -0500

    added AJAX  post to controller and JS files

--- controllers/root_controller.rb
+++ controllers/root_controller.rb
@@ -4,6 +4,20 @@ class RootController < ApplicationController
   end
 
   post('/') do
-
+    input_array = format_input(params[:text])
+    check_dictionary
   end
+
+  def format_input(text)
+    text.gsub(/[?.,:;'"!&()]/, "").split(" ")
+  end
+
+  def check_dictionary
+    dict = []
+    File.open('./public/images/dictionary.txt').each do |row|
+      dict.push(row.strip.downcase)
+    end
+    dict
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,12 @@
 ENV['RACK_ENV'] = 'test'
+require 'rubygems'
+require 'bundler'
+Bundler.require(:default, ENV['RACK_ENV'])
+require './config/boot'
 
-require 'rspec'
-require 'capybara/rspec'
-# require './app'
-# Capybara.app = App
+# For multiple routes
+config_routes = File.expand_path('../../config.ru', __FILE__)
+Capybara.app = Rack::Builder.parse_file(config_routes).first
 
 RSpec.configure do |config|
   config.include Capybara::DSL
