d6b48d9b1db167c5a194e54bb6da1ad5e3e73f99
ericak11 <kantor.erica@gmail.com>
2014-11-30 14:38:23 -0500

    condenced dictionary and added more regex to make sure words are being checked correctly

--- controllers/root_controller.rb
+++ controllers/root_controller.rb
@@ -6,7 +6,7 @@ class RootController < ApplicationController
   post('/') do
     results = check_dictionary(params[:text])
     total_words = results[1] + results[2]
-    accuracy  = (results[1]/total_words.to_f) * 100
+    accuracy  = ((results[1]/total_words.to_f) * 100).round(2)
     phrase = results[0]
     {phrase: phrase, accuracy: accuracy, incorrect: results[2], correct: results[1], total: total_words}.to_json
   end
@@ -17,15 +17,24 @@ class RootController < ApplicationController
     incorrect = 0
     dict = Hash.new{|hash, key| hash[key] = []}
     File.open('public/images/dictionary.txt').each do |row|
-      word = row.strip.downcase
+      word = row.encode('UTF-8', 'binary', invalid: :replace, undef: :replace, replace: '').strip.gsub(/\/[^\s]+/,"").downcase
       first_letter =  word[0,1]
       dict[first_letter] << word
     end
     text.split(" ").each do |word|
-      adjusted_word = word.gsub(/[?.,:;'"!&()]/, "").downcase
+      adjusted_word = word.gsub(/[?.,:;"!&()‟”“’‛‘'*-]/, "").downcase
+      substituted_word = word.gsub(/[’‛]/, "'").downcase
       if dict[adjusted_word[0]].include? adjusted_word
         phrase += "#{word} "
         correct += 1
+      elsif dict[adjusted_word[0]].include? substituted_word
+        phrase += "#{word} "
+        correct += 1
+      elsif adjusted_word.count("0-9") > 0
+        phrase += "#{word} "
+        correct += 1
+      elsif  adjusted_word == ""
+        phrase += "#{word} "
       else
         phrase += "<span class='wrong'>#{word}</span> "
         incorrect += 1
